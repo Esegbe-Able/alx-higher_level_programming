d1b5cb14efd7917890914ed90b9cebca79d880c9
ebeatsonaffram <enyonambeatsonaffram@gmail.com>
2024-05-29 22:34:29 +0000

    jquesry

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,3 @@
+// Update text color of the HTML HEADER tag to red
+
+document.querySelector('head').style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,3 @@
+// Use jQuery API to change header tag color to red
+
+$('header').css('color', '#FF0000');
